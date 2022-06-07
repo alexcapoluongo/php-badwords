fd0d9551d2ff90a2b716d0a8f19bf52a3955dcac
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-07 14:11:14 +0200

    comments removed

--- index.php
+++ index.php
@@ -6,11 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words Censured</title>
 </head>
-<!-- Descrizione:
-[*] Creare una variabile con un paragrafo di testo a vostra scelta.
-[*] Stampare a schermo il paragrafo e la sua lunghezza.
-[*] Una parola da censurare viene passata dall'utente tramite parametro GET.
-[] Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 <body>
     <?php
     $paragraph = "
